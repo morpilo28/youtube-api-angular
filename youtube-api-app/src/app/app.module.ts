12fb774ab717acfc9e35306ae35f65c4ec4f18fc
mor <odkarit22@gmail.com>
2020-01-16 09:57:02 +0200

    ordering folders

--- youtube-api-app/src/app/app.module.ts
+++ youtube-api-app/src/app/app.module.ts
@@ -5,9 +5,9 @@ import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PlaylistComponent } from './components/playlist/playlist.component';
-import { PlayerComponent } from './components/player/player.component';
-import { SearchComponent } from './components/search/search.component';
+import { PlaylistComponent } from './components/search-page/playlist/playlist.component';
+import { PlayerComponent } from './components/search-page/player/player.component';
+import { SearchComponent } from './components/search-page/search/search.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { SearchPageComponent } from './components/search-page/search-page.component';
