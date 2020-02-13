ac3d89adf9c2dc5f79769106f4cddda4d174b294
morpilo28 <51907906+morpilo28@users.noreply.github.com>
2020-02-13 08:42:46 +0200

    Update environment.prod.ts

--- youtube-api-app/src/environments/environment.prod.ts
+++ youtube-api-app/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   serverUri: 'http://localhost:3201', // only when going to local server
-  apiKey: 'AIzaSyAEusu_4PwMPkU9F4E10B83tk08DycNPBU',
+  apiKey: '', //TODO: add youtube api key
   youTubeBaseUrl: /* 'https://www.googleapis.com/youtube/v3' */'http://localhost:4200',
 };
 
